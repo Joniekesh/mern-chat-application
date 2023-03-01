03ad186e0c059c2833f904927cc3b54951860c90
okoro john darlington <okorojohndarlington@gmail.com>
2023-03-01 10:22:58 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	pingTimeout: 60000,
-	cors: { origin: "http://localhost:5173" },
+	cors: { origin: "https://joniechat.netlify.app/" },
 });
 
 io.on("connection", (socket) => {
